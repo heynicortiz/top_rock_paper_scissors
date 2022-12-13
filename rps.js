d00dc15e57089629e7a5d2440263771c093af533
Nic Ortiz <ortiz.nagare@gmail.com>
2022-12-12 19:57:25 -0500

    cleaning up code segments, improving design

--- rps.js
+++ rps.js
@@ -3,20 +3,19 @@ const playerScoreDiv = document.querySelector("#playerScore .score");
 const makeYourSelection = document.querySelector("#makeYourSelection");
 const declare = document.querySelector("#rulesVictory");
 const inputChoices = document.querySelector("#inputChoices");
-const mainContainer = document.querySelector("#mainContainer");
 const repeat = document.querySelector("#repeat");
 const computerScoreDiv = document.querySelector("#computerScore .score");
 const playerCurrent = document.querySelector("#playerCurrent .fai-large");
 const computerCurrent = document.querySelector("#computerCurrent .fai-large");
 const playerHistory = document.querySelector("#playerHistory");
 const computerHistory = document.querySelector("#computerHistory");
+const playerOptions = document.querySelectorAll(".inputButton");
 
-// initial both scores
+// initialize both scores
 let playerScore = 0;
 let computerScore = 0;
 
 // activate buttons
-const playerOptions = document.querySelectorAll(".inputButton");
 playerOptions.forEach((option) => {
     option.addEventListener("click", () => {
         playRound(option.id, getComputerChoice());
@@ -48,9 +47,8 @@ function playRound(playerSelection, computerSelection) {
         computerScoreDiv.textContent = computerScore.toString();
     }
 
-
     if (playerScore >= 5 || computerScore >= 5) {
-        declare.classList.add("blue", "f2");
+        declare.classList.add("f2");
         declare.classList.remove("f4");
         makeYourSelection.classList.add("dn");
         inputChoices.classList.add("dn");
@@ -76,17 +74,11 @@ function playRound(playerSelection, computerSelection) {
             playerHistory.innerHTML = "";
             computerHistory.innerHTML = "";
             repeat.classList.replace("db", "dn");
-
         });
     }
 }
 
-function game() {
-
-}
-
 // updating various parts of the page
-
 function selectionsMade(playerSelection, computerSelection) {
 
     // add icon to each side's history
@@ -94,39 +86,41 @@ function selectionsMade(playerSelection, computerSelection) {
     createIcon(computerSelection, "computer");
 
     // remove the existing classes first
-    playerCurrent.removeAttribute("class");
-    computerCurrent.removeAttribute("class");
+    playerCurrent.classList.remove("fa-hand-rock", "fa-hand-paper", "fa-hand-scissors", "fa-user-astronaut");
+    computerCurrent.classList.remove("fa-hand-rock", "fa-hand-paper", "fa-hand-scissors", "fa-robot");
 
     // then depending on the selections, add new classes as appropriate
     switch (playerSelection) {
         case "rock":
-            playerCurrent.classList.add("far", "fai-large", "fa-hand-rock");
+            playerCurrent.classList.add("fa-hand-rock");
             break;
         case "paper":
-            playerCurrent.classList.add("far", "fai-large", "fa-hand-paper");
+            playerCurrent.classList.add("fa-hand-paper");
             break;
         case "scissors":
-            playerCurrent.classList.add("far", "fai-large", "fa-hand-scissors");
+            playerCurrent.classList.add("fa-hand-scissors");
             break;
     }
 
     switch (computerSelection) {
         case "rock":
-            computerCurrent.classList.add("far", "fai-large", "fa-hand-rock");
+            computerCurrent.classList.add("fa-hand-rock");
             break;
         case "paper":
-            computerCurrent.classList.add("far", "fai-large", "fa-hand-paper");
+            computerCurrent.classList.add("fa-hand-paper");
             break;
         case "scissors":
-            computerCurrent.classList.add("far", "fai-large", "fa-hand-scissors");
+            computerCurrent.classList.add("fa-hand-scissors");
             break;
     }
 }
 
+// creates a FontAwesome icon and adds to the appropriate side
 function createIcon(selection, side) {
     let icon = document.createElement("i");
-    icon.classList.add("far", "pa1", "fai-previous");
+    icon.classList.add("far", "pa1", "fai-previous", "fa-fw");
 
+    // create appropriate icon based on selection
     switch (selection) {
         case "rock":
             icon.classList.add("fa-hand-rock");
@@ -139,6 +133,7 @@ function createIcon(selection, side) {
             break;
     }
 
+    // add it to either player or computer history
     switch (side) {
         case "player":
             playerHistory.appendChild(icon);
@@ -149,4 +144,3 @@ function createIcon(selection, side) {
     }
 
 }
-game();
